f7c2b4dd0c49493084d0e54444efc0c6b647c6c4
Sanya-frontend <79463917+Sanya-frontend@users.noreply.github.com>
2021-03-15 01:30:55 +0300

    feat: add new function

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  if (!matrix || matrix.length == 0) 
-  return [];
+  if (!matrix || matrix.length == 0) return [];
   let array = [];
   let num = 0;
-  for (let i=0; i<matrix.length; i++) {
-    if (i%2 != 0)
+  for (let i = 0; i<matrix.length; i++) {
+    if (i % 2 != 0)
     {
       for (let n = matrix[i].length - 1; n > -1; n--) {
         array[num] = matrix[i][n];
@@ -15,7 +14,7 @@ module.exports = function towelSort (matrix) {
       }
     }
       else
-      for (let j = 0; j < matrix[i].length; n++) {
+      for (let n = 0; n < matrix[i].length; n++) {
         array[num]= matrix[i][n];
         num++;
       }
